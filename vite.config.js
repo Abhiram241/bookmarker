ca1077e3718d1885eab853534976cf72c46c71ac
Abhiram241 <yadatoreabhiram@gmail.com>
2025-05-26 19:49:52 +0530

    check 123

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { webcrypto } from 'crypto'
 
-// Polyfill globalThis.crypto for environments lacking it
 if (!globalThis.crypto) {
   globalThis.crypto = webcrypto
 }
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
 })
