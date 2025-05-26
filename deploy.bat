b24443f71a3dd9d763136b08f000cc8e5f9f7702
Abhiram241 <yadatoreabhiram@gmail.com>
2025-05-26 19:26:16 +0530

    check 123

---
+++ deploy.bat
@@ -0,0 +1,4 @@
+@echo off
+git add .
+git commit -m "check 123"
+git push
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { webcrypto } from "crypto";
 
+// Patch: Fix getRandomValues() for Vite build in Node.js
+if (!globalThis.crypto) {
+  globalThis.crypto = webcrypto;
+}
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+  plugins: [react()],
+});
